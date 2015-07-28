bdd4fa9710f152f0a5ef9eaa9171514df5689f85
Atul Ganatra <ganatraa@amazon.com>
2015-07-28 14:57:38 -0400

    test

---
+++ src/test/java/com/aws/wwps/demo/MessageProcessorTest.java
@@ -0,0 +1,39 @@
+package com.aws.wwps.demo;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class MessageProcessorTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public MessageProcessorTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( MessageProcessorTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+    	assertEquals("stuff", MessageProcessor.someCall());
+    }
+
+}
